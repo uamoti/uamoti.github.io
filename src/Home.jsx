bcca1797961bb7c6923d03641bc2c9188d366964
uamoti <uamoti@anche.no>
2026-02-08 18:52:19 +0200

    refactor: add avatar to Home

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,11 +1,20 @@
+import brutalLegendImage from './assets/brutal-legend-2.png';
+
 function Home() {
     return (
-	<div>
-            <p>Hi there,</p>
-	    <p>Thanks for visiting my humble corner of the web.</p>
-	    <p>I'm Bernardo, a full stack student at kood/Sisu.</p>
-		<p>I'm interested in back end development and automation.
-		Here you can follow a bit of my journey from biomedical researcher to coding wizard.</p>
+	<div className="home-content">
+	    <div className="logo">
+		<img src={brutalLegendImage} alt="Avatar of a headbanger"/>
+	    </div>
+	    <div>
+		<p>Hi there,</p>
+		<p>Thanks for visiting my humble corner of the web.</p>
+		<p>I'm Bernardo, a full stack student at kood/Sisu.</p>
+		    <p>
+			I'm interested in back end development and automation.
+			Here you can follow a bit of my journey from biomedical researcher to coding wizard.
+		    </p>
+	    </div>
 	</div>
     )
 }
