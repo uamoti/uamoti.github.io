34bf42cd3d8a19e2ce6a23981ee859c4a9b85622
uamoti <uamoti@anche.no>
2026-02-16 18:02:39 +0200

    feat: add academic history overview

--- src/Academia.jsx
+++ src/Academia.jsx
@@ -1,8 +1,49 @@
 function Academia() {
     return (
-        <div>
+        <div className="home-content">
             <h2>Academia</h2>
-            <p>Denna sida är under uppbyggnad.</p>
+            <p>
+		It started with the desire to understand the process of muscle hypertrophy
+		and hopefully modulate it. Although I had joined the laboratory of exercise
+		physiology at UFMG, that theme wasn't on the table, so I had to go along the
+		lab themes of fatigue and thermoregulation.
+	    </p>
+	    <br/>
+	    <p>
+		Once my bachelors was done, I scored a position in the masters programme of
+		biology of physical activity at the University of Jyväskylä (Finland).
+		I had the pleasure and honour of having Juha Hulmi as my supervisor, a very
+		knowledgeable guy in muscle matters &#x1F4AA;
+	    </p>
+	    <br/>
+	    <p>
+		After some months back home, I managed to secure a scholarship from the
+		<i>Science Without Borders</i> programme from the Brazilian Ministry
+		of Education. And so we went to Lund (Sweden) to continue working on muscle
+		dystrophies, this time congenital instead of Duchenne, and microRNAs.
+	    </p>
+	    <br/>
+	    <p>
+		Continuing on the theme of non-coding RNAs, I moved to Odense (Denmark) for
+		my first post-doc stint, this time working with lncRNAs and obesity. I got to
+		learn a lot of molecular biology and the basics of cell culture.
+	    </p>
+	    <br/>
+	    <p>
+		My last post-doc was at the University of Eastern Finland, working on cell
+		cycle and leukaemia. I got to explore some CRISPR techniques for genetic
+		engineering and expanded my skills in cell culture.
+	    </p>
+	    <br/>
+	    <p>
+		All this journey was every now and then spiced up with some bioinformatics
+		work, which reinforced my passion for coding. Although there are many
+		interesting things in bioinfo, I still see it largely as data science applied
+		to biology; and I'm more of a software developer. And it's really nice to see
+		more positions for software developers appearing at universities and research
+		groups and facilities. Who knows? I might be able to combine the best of both
+		worlds &#x1F60E;
+	    </p>
         </div>
     )
 }
